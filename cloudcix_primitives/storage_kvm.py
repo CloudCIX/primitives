52662e896d499cc6fe8d06f422b27632098f4b48
kn@cix.ie <kn@cix.ie>
2024-09-26 16:09:02 +0530

    added read command in build of storage_kvm primitive to make sure the build doesn't overwrite the existing file

--- cloudcix_primitives/storage_kvm.py
+++ cloudcix_primitives/storage_kvm.py
@@ -59,8 +59,10 @@ def build(
     # Define message
     messages = {
         1000: f'1000: Successfully created storage {storage}',
-        3021: f'3021: Failed to connect to the host {host} for the payload create_storage_file',
+        3021: f'3021: Failed to connect to the host {host} for the payload read_storage_file',
         3022: f'3022: Failed to create storage_kvm {domain_path}{storage} on the host {host}'
+        3023: f'3023: Failed to connect to the host {host} for the payload create_storage_file',
+        3024: f'3024: Failed to create storage_kvm {domain_path}{storage} on the host {host}'
     }
 
     def run_host(host, prefix, successful_payloads):
@@ -72,15 +74,25 @@ def build(
         )
 
         payloads = {
+            'read_storage_file': f'qemu-img info {domain_path}{storage}',
             'create_storage_file': f'qemu-img create -f qcow2 {domain_path}{storage} {size}G',
         }
 
-        ret = rcc.run(payloads['create_storage_file'])
+        create_storage_file = True
+        ret = rcc.run(payloads['read_storage_file'])
         if ret["channel_code"] != CHANNEL_SUCCESS:
             return False, fmt.channel_error(ret, messages[prefix + 1]), fmt.successful_payloads
-        if ret["payload_code"] != SUCCESS_CODE:
-            return False, fmt.payload_error(ret, messages[prefix + 2]), fmt.successful_payloads
-        fmt.add_successful('create_storage_file', ret)
+        if ret["payload_code"] == SUCCESS_CODE:
+            create_storage_file = False
+            fmt.add_successful('read_storage_file', ret), fmt.successful_payloads
+
+        if create_storage_file:
+            ret = rcc.run(payloads['create_storage_file'])
+            if ret["channel_code"] != CHANNEL_SUCCESS:
+                return False, fmt.channel_error(ret, messages[prefix + 3]), fmt.successful_payloads
+            if ret["payload_code"] != SUCCESS_CODE:
+                return False, fmt.payload_error(ret, messages[prefix + 4]), fmt.successful_payloads
+            fmt.add_successful('create_storage_file', ret)
 
         return True, "", fmt.successful_payloads
 
@@ -266,6 +278,9 @@ def read(
     }
 
     def run_host(host, prefix, successful_payloads):
+        retval = True
+        data_dict[host] = {}
+
         rcc = SSHCommsWrapper(comms_ssh, host, 'robot')
         fmt = HostErrorFormatter(
             host,
