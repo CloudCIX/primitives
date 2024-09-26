8057f7843654a87856846cc973911e74f01778e7
kn@cix.ie <kn@cix.ie>
2024-09-26 21:43:35 +0530

    bug fixes

--- cloudcix_primitives/storage_kvm.py
+++ cloudcix_primitives/storage_kvm.py
@@ -60,7 +60,7 @@ def build(
     messages = {
         1000: f'1000: Successfully created storage {storage}',
         1001: f'1001: Already a storage {storage} of size {size}GBs file exists at {domain_path}',
-        3001: f'3001: Already a storage {storage} of different size than requested size {size} '
+        3001: f'3001: Already a storage {storage} of different size than requested size {size}GBs '
               f'file exists at {domain_path}',
         3021: f'3021: Failed to connect to the host {host} for the payload read_storage_file',
         3022: f'3022: Failed to connect to the host {host} for the payload create_storage_file',
