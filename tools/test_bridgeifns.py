9d246ee826dcb2ea44ef0d0158c3410505eb8e6f
B-Tulgat <ttulgat@gmail.com>
2024-10-01 19:43:24 +0100

    fixed test-bridgeifns

--- tools/test_bridgeifns.py
+++ tools/test_bridgeifns.py
@@ -1,40 +1,79 @@
-#!/usr/bin/python3
-
-import json
+amon@LAPTOP-J76M3340:~/work/cloudcix-work/primitives$ docker ps
+CONTAINER ID   IMAGE     COMMAND       CREATED       STATUS       PORTS
+     NAMES
+a4523998db29   ubuntu    "/bin/bash"   13 days ago   Up 5 hours
+     happy_shamir
+amon@LAPTOP-J76M3340:~/work/cloudcix-work/primitives$ docker exec -it a
+4 /bin/bash
+(base) root@a4523998db29:/# ls
+bin                home               mnt   sbin                usr
+bin.usr-is-merged  lib                opt   sbin.usr-is-merged  var
+boot               lib.usr-is-merged  proc  srv
+dev                lib64              root  sys
+etc                media              run   tmp
+(base) root@a4523998db29:/# cd home/ubuntu/
+.bash_logout  .bashrc       .profile      test/
+(base) root@a4523998db29:/# cd home/ubuntu/test/
+(base) root@a4523998db29:/home/ubuntu/test# ls
+bridgeifns.py  primitives
+(base) root@a4523998db29:/home/ubuntu/test# cd primitives/
+(base) root@a4523998db29:/home/ubuntu/test/primitives# ls
+DEVELOPMENT.md  README.md      cloudcix_primitives  setup.py
+LICENSE         __pycache__    requirements.txt     test_bridgeifns.py
+MANIFEST.in     bridgeifns.py  routens.py           tools
+(base) root@a4523998db29:/home/ubuntu/test/primitives# vim test_bridgei
+fns.py
+(base) root@a4523998db29:/home/ubuntu/test/primitives# cat test_bridgei
+fns.py
 import sys
-
+import json
 from cloudcix_primitives import bridgeifns
 
-cmd = sys.argv[1]
-
+# Fetch command and arguments
+cmd = sys.argv[1] if len(sys.argv) > 1 else None
 bridgename, namespace_name = "testbridge", "testns"
 
 if len(sys.argv) > 2:
     bridgename = sys.argv[2]
-    namespace_name = sys.argv[2]
+if len(sys.argv) > 3:
+    namespace_name = sys.argv[3]
 
+status = None
+msg = None
+(base) root@a4523998db29:/home/ubuntu/test/primitives# python bridgeifn
+s.py scrub frodo sam
+(base) root@a4523998db29:/home/ubuntu/test/primitives# python bridgeifn
+s.py read frodo sam
+if len(sys.argv) > 2:
+    bridgename = sys.argv[2]
+if len(sys.argv) > 3:
+    namespace_name = sys.argv[3]
 
 status = None
 msg = None
 data = None
 
+# Check and execute command
 if cmd == 'build':
-    status, msg = bridgeifns.build(bridgename, namespace_name, "169.254.169.254", "/etc/cloudcix/pod/configs/config.json")
-if cmd == 'scrub':
+    status, msg = bridgeifns.build(bridgename, namespace_name, "/etc/cloudcix/pod/configs/config.json")
+elif cmd == 'scrub':
     status, msg = bridgeifns.scrub(bridgename, namespace_name, "/etc/cloudcix/pod/configs/config.json")
-if cmd == 'read':
-    status, data, msg = bridgeifns.read(bridgename, namespace_name, "169.254.169.254", "/etc/cloudcix/pod/configs/config.json")
+elif cmd == 'read':
+    status, data, msg = bridgeifns.read(bridgename, namespace_name,  "/etc/cloudcix/pod/configs/config.json")
+else:
+   print(f"Unknown command: {cmd}")
+   sys.exit(1)
 
-print("Status: %s" %  status)
-print()
-print("Message:")
-if type(msg) == list:
+# Output the status and messages
+print("Status: %s" % status)
+print("\nMessage:")
+if isinstance(msg, list):
     for item in msg:
         print(item)
 else:
     print(msg)
 
+# Output data if available
 if data is not None:
-    print()
-    print("Data:")
+    print("\nData:")
     print(json.dumps(data, sort_keys=True, indent=4))
