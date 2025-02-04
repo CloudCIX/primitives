41f06623e4c63e93290b96c944cc02b062deed49
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2025-02-04 15:38:07 +0000

    update test comment to mention ipv6 specifics

--- tools/test_geo_b_firewall_ns.py
+++ tools/test_geo_b_firewall_ns.py
@@ -11,13 +11,14 @@ from cloudcix_primitives import geo_b_firewall_ns
 #
 #   tools/test_ns.py build ns1100
 #   tools/test_bridgeif_ns.py build br-B1 ns1100
-#   Create one or more nftable sets with "ip netns exec ns1100 nft add set inet FILTER <name> '{ type ipv4_addr; flags interval; auto-merge; }'"
+#   Create one or more nftable sets with "ip netns exec ns1100 nft add set inet FILTER <name>_V4 '{ type ipv4_addr; flags interval; auto-merge; }'",
+#   replacing V4 with V6 and ipv4_addr with ipv6_addr to test ipv6 path.
 
 cmd = sys.argv[1] if len(sys.argv) > 1 else None
 namespace_name = 'ns1100'
 # these reference ipv4 sets that need to exist beforehand
-inbound = ['IE_V4', 'GB_V5']
-outbound = ['US_V1', 'JP_V2']
+inbound = ['IE_V4', 'GB_V6']
+outbound = ['US_V6', 'JP_V4']
 config_file = '/etc/cloudcix/pod/configs/config.json'
 
 if len(sys.argv) > 2:
