c60445e23b5e9c56bfe47c70a96d309ebcf9949c
Eóin <99229563+gysahlgreene@users.noreply.github.com>
2025-07-23 03:59:22 -0700

    LXD - VM Support (#126)
    * Refactor LXD Primitives to now support both containers and virtual machines.
    
    - Modified function parameters and internal messages to use "instance_name" instead of "container_name".
    - Adjusted API calls from 'containers.get' to 'instances.get' All functions now use the unified instances API.
    - Ensured all success and error messages are updated to reference instances correctly.
    
    * confirm VM support working with primitives
    
    * added instance_type param
    
    docstring tidyup
    
    * instance_type - added to return messages
    
    * consistency update
    
    * catching remaining references to `containers` and replacing with `instance`

--- cloudcix_primitives/ram_lxd.py
+++ cloudcix_primitives/ram_lxd.py
@@ -14,25 +14,51 @@ __all__ = [
 def update(
         endpoint_url: str,
         project: str,
-        container_name: str,
+        instance_name: str,
         ram: int,
+        instance_type: str,
         verify_lxd_certs: bool = True
 ) -> Tuple[bool, str]:
-    
-    """ Update the RAM of an LXD instance.
-    :param endpoint_url: The endpoint URL for the LXD Host.
-    :param project: The LXD project name.
-    :param instance_name: The name of the LXD instance.
-    :param ram: The amount of RAM to set, in GB.
-    :param verify_lxd_certs: Boolean to verify LXD certs.
-    :return: A tuple with a boolean flag indicating success or failure, a message, and a dictionary of successful payloads.
+    """
+    description:
+        Update the RAM of an LXD instance.
+
+    parameters:
+        endpoint_url:
+            description: The endpoint URL for the LXD Host.
+            type: string
+            required: true
+        project:
+            description: The LXD project name.
+            type: string
+            required: true
+        instance_name:
+            description: The name of the LXD instance.
+            type: string
+            required: true
+        ram:
+            description: The amount of RAM to set, in GB.
+            type: integer
+            required: true
+        instance_type:
+            description: The type of LXD instance, either 'vms' or 'containers'.
+            type: string
+            required: true
+        verify_lxd_certs:
+            description: Boolean to verify LXD certs.
+            type: boolean
+            required: false
+
+    return:
+        description: A tuple with a boolean flag indicating success or failure and a message.
+        type: tuple
     """
     # Define message
     messages = {
-        1000: f'Successfully updated the RAM for containers {container_name} on {endpoint_url}',
-        3021: f'Failed to connect to {endpoint_url} for containers.get payload',
-        3022: f'Failed to run containers.get payload on {endpoint_url}. Payload exited with status ',
-        3023: f'Failed to update the RAM for containers {container_name}. Error: ',
+        1000: f'Successfully updated the RAM for {instance_type} {instance_name} on {endpoint_url}',
+        3021: f'Failed to connect to {endpoint_url} for instances.get payload',
+        3022: f'Failed to run instances.get payload on {endpoint_url}. Payload exited with status ',
+        3023: f'Failed to update the RAM for instance {instance_name}. Error: ',
     }
 
     def run_host(endpoint_url, prefix, successful_payloads):
@@ -44,20 +70,20 @@ def update(
         )
         
         # Get the instance
-        ret = rcc.run(cli='containers.get', name=container_name)
+        ret = rcc.run(cli='instances.get', name=instance_name)
         if ret["channel_code"] != CHANNEL_SUCCESS:
             return False, fmt.channel_error(ret, f"{prefix+1}: {messages[prefix+1]}"), fmt.successful_payloads
         if ret["payload_code"] != API_SUCCESS:
             return False, fmt.payload_error(ret, f"{prefix+2}: {messages[prefix+2]}"), fmt.successful_payloads
 
         instance = ret['payload_message']
-        fmt.add_successful('containers.get', ret)
+        fmt.add_successful('instances.get', ret)
             
         # Update the memory limit
         try:
             instance.config['limits.memory'] = f'{ram}GB'
             instance.save(wait=True)
-            fmt.add_successful('containers.set', {'limits.memory': f'{ram}GB'})
+            fmt.add_successful('instances.set', {'limits.memory': f'{ram}GB'})
         except Exception as e:
             return False, f"{prefix+3}: {messages[prefix+3]}: {e}", fmt.successful_payloads
         return True, '', fmt.successful_payloads
