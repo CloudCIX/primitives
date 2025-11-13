58c301b056eee0efaf93a6aa2240e7dcf5a1cc95
gysahlgreene <eoin.macd95@gmail.com>
2025-11-13 16:24:50 +0000

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloudcix_primitives",
-    version='1.0.0',
+    version='1.0.1',
     author="CloudCIX",
     author_email="developers@cloudcix.com",
     maintainer="CloudCIX",
